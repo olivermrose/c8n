a3ce54f1fdaf491b1f57048785ae8fe38e32e4e0
Oliver Rose <hello@olivermrose.com>
2026-03-03 01:54:50 -0500

    chore: ignore internal file when testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		include: ["{test,src}/**/*.ts"],
-		exclude: ["src/index.ts"],
+		include: ["src/**/*.ts"],
+		exclude: ["src/index.ts", "src/_internal.ts"],
 		coverage: {
 			enabled: true,
 		},
